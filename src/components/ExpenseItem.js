2c455d6771b7527e53f6a32af0f5219625574482
athiratj <athiratjthanaly83.com>
2023-04-07 16:54:21 +0530

    Add Expenses component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){
 
     return(
         <div className='expense-item'>
             {/* Error occured for date solved: https://bobbyhadz.com/blog/react-objects-are-not-valid-as-react-child */}
-            <div>{props.date.toLocaleDateString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
