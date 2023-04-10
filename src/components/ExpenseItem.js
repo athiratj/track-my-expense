1662baf77b564ed3cc88f582742efb3d486645b5
athiratj <athiratjthanaly83.com>
2023-04-10 15:59:45 +0530

    Adding useState hook for event handling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 
 function ExpenseItem(props){
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+    }
 
     return(
         <Card className='expense-item'>
             {/* Error occured for date solved: https://bobbyhadz.com/blog/react-objects-are-not-valid-as-react-child */}
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Item</button>
         </Card>
     );
 }
