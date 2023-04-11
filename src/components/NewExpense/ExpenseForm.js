dfd0161f73910e98fa3f3e654170f58acaa97544
athiratj <athiratjthanaly83@gmail.com>
2023-04-11 13:22:08 +0530

    2 way binding to clear form after submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -39,6 +39,9 @@ function ExpenseForm(){
             date : new Date (enteredDate),
         };
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredPrice('');
+        setEnteredDate('');
     }
 
     return(
@@ -46,15 +49,15 @@ function ExpenseForm(){
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={priceChangeHandler} />
+                    <input type="number" min="0.01" step="0.01" value={enteredPrice} onChange={priceChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-12-01" max="2022-12-31" onChange={dateChangeHandler} />
+                    <input type="date" min="2019-12-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
